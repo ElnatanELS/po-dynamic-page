532387a6606da08bd316f5210d891c3d51dce4c3
Elnatan Emanuel Lima Santos <elnatangalaxy@gmail.com>
2024-03-11 11:28:26 -0300

    feat: create page list people

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { PageListComponent } from './pages/page-list/page-list.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,17 +7,21 @@ import { AppComponent } from './app.component';
 import { PoModule } from '@po-ui/ng-components';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { PoTemplatesModule } from '@po-ui/ng-templates';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     PoModule,
     HttpClientModule,
-    RouterModule.forRoot([])
+    RouterModule.forRoot([]),
+    PoTemplatesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
