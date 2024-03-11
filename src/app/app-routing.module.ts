868e7d47f0beba08780118939f637d44e91324ac
Elnatan Emanuel Lima Santos <elnatangalaxy@gmail.com>
2024-03-11 13:35:08 -0300

    feat: ajuste de busca avançada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { PageListComponent } from './pages/page-list/page-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'people', component:PageListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
